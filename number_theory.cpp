ecba315d191c9f19da30f850f42c90f48f885e13
abhishekvtangod <abhishekvtangod@gmail.com>
2021-03-18 22:02:18 +0530

    number_theory

---
+++ number_theory.cpp
@@ -0,0 +1,73 @@
+/**
+ *    author:  abhishekvtangod
+**/
+// #undef _GLIBCXX_DEBUG
+// #undef _ABHI
+#include<bits/stdc++.h>
+using namespace std; 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// oset<pair<ll,ll>> indexed_set;
+
+#define mod 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define bits(x) __builtin_popcountll(x)
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long int ll;
+
+void debug_out() { cerr << "]" << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << H << ", ";
+  debug_out(T...);
+}
+
+#ifdef _GLIBCXX_DEBUG
+#define debug(x...) cerr << "[" << #x << "]:[", debug_out(x)
+#else
+#define debug(...) 42
+#endif
+
+struct number_theory{
+	int gcd(int x, int y){
+		if(!x){
+			return y;
+		} else if(!y){
+			return x;
+		}
+
+		return gcd(y, x%y);
+	}
+
+}
+
+
+void solve(){
+
+}
+
+
+int main()
+{   
+	IOS;
+
+ 	#ifdef _ABHI
+		freopen("/home/abhi/Documents/input.txt", "r", stdin);
+		freopen("/home/abhi/Documents/output.txt", "w", stdout);
+	#endif 
+ 
+
+	ll t=1;
+	// cin>>t;
+	while(t--){
+		solve();
+	}
+	
+
+    return 0;
+}
